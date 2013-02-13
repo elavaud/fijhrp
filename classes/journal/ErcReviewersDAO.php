64806469aa34befef6af15fe2cba801fc689d212
Edouard Lavaud <edouard.lavaud@gmail.com>
2013-02-13 12:21:28 +0100

    New DAO for the erc reviewers

---
+++ classes/journal/ErcReviewersDAO.php
@@ -0,0 +1,152 @@
+<?php
+
+/**
+ * @file classes/journal/ErcReviewersDAO.inc.php
+ *
+ *
+ * @class ErcReviewersDAO
+ * @ingroup journal
+ *
+ * @brief Class for DAO relating erc to reviewers.
+ */
+
+// $Id$
+define('ERC_STATUS_CHAIR', 1);
+define('ERC_STATUS_VICE_CHAIR',	2);
+define('ERC_STATUS_MEMBER', 3);
+
+class ErcReviewersDAO extends DAO {
+	
+	/**
+	 * Insert a new erc reviewers.
+	 * @param $journalId int
+	 * @param $sectionId int
+	 * @param $userId int
+	 * @param $status int
+	 */
+
+	function insertReviewer($journalId, $sectionId, $userId, $status) {
+		return $this->update(
+			'INSERT INTO erc_reviewers
+				(journal_id, section_id, user_id, status)
+				VALUES
+				(?, ?, ?, ?)',
+			array(
+				$journalId,
+				$sectionId,
+				$userId,
+				$status
+			)
+		);
+	}
+
+	/**
+	 * Delete an erc_reviewer.
+	 * @param $journalId int
+	 * @param $sectionId int
+	 * @param $userId int
+	 */
+	function deleteReviewer($journalId, $sectionId, $userId) {
+		return $this->update(
+			'DELETE FROM erc_reviewers WHERE journal_id = ? AND section_id = ? AND user_id = ?',
+			array(
+				$journalId,
+				$sectionId,
+				$userId
+			)
+		);
+	}
+
+	/**
+	 * Retrieve a list of all erc reviewers assigned to the specified erc.
+	 * @param $journalId int
+	 * @param $sectionId int
+	 * @return array matching Users
+	 */
+	function &getReviewersBySectionId($journalId, $sectionId) {
+		$users = array();
+
+		$userDao =& DAORegistry::getDAO('UserDAO');
+
+		$result =& $this->retrieve(
+			'SELECT u.*, er.status FROM users AS u, erc_reviewers AS er WHERE u.user_id = er.user_id AND er.journal_id = ? AND er.section_id = ? ORDER BY last_name, first_name',
+			array($journalId, $sectionId)
+		);
+
+		while (!$result->EOF) {
+			$row = $result->GetRowAssoc(false);
+			$users[] = array(
+				'user' => $userDao->_returnUserFromRow($row),
+				'status' => $row['status']
+			);
+			$result->moveNext();
+		}
+
+		$result->Close();
+		unset($result);
+
+		return $users;
+	}
+
+	/**
+	 * Delete all erc reviewers for a specified erc in a journal.
+	 * @param $sectionId int
+	 * @param $journalId int
+	 */
+	function deleteReviewersBySectionId($sectionId, $journalId = null) {
+		if (isset($journalId)) return $this->update(
+			'DELETE FROM erc_reviewers WHERE journal_id = ? AND section_id = ?',
+			array($journalId, $sectionId)
+		);
+		else return $this->update(
+			'DELETE FROM erc_reviewers WHERE section_id = ?',
+			$sectionId
+		);
+	}
+
+	/**
+	 * Delete all erc reviewers for a specified journal.
+	 * @param $journalId int
+	 */
+	function deleteReviewersByJournalId($journalId) {
+		return $this->update(
+			'DELETE FROM erc_reviewers WHERE journal_id = ?', $journalId
+		);
+	}
+
+	/**
+	 * Delete all section assignments for the specified user.
+	 * @param $userId int
+	 * @param $journalId int optional, include assignments only in this journal
+	 * @param $sectionId int optional, include only this section
+	 */
+	function deleteReviewersByUserId($userId, $journalId  = null, $sectionId = null) {
+		return $this->update(
+			'DELETE FROM erc_reviewers WHERE user_id = ?' . (isset($journalId) ? ' AND journal_id = ?' : '') . (isset($sectionId) ? ' AND section_id = ?' : ''),
+			isset($journalId) && isset($sectionId) ? array($userId, $journalId, $sectionId)
+			: (isset($journalId) ? array($userId, $journalId)
+			: (isset($sectionId) ? array($userId, $sectionId) : $userId))
+		);
+	}
+
+	/**
+	 * Check if a user is assigned to a specified section.
+	 * @param $journalId int
+	 * @param $sectionId int
+	 * @param $userId int
+	 * @return boolean
+	 */
+	function reviewerExists($journalId, $sectionId, $userId) {
+		$result =& $this->retrieve(
+			'SELECT COUNT(*) FROM erc_reviewers WHERE journal_id = ? AND section_id = ? AND user_id = ?', array($journalId, $sectionId, $userId)
+		);
+		$returner = isset($result->fields[0]) && $result->fields[0] == 1 ? true : false;
+
+		$result->Close();
+		unset($result);
+
+		return $returner;
+	}
+}
+
+?>
--- templates/manager/index.tpl
+++ templates/manager/index.tpl
@@ -78,7 +78,8 @@
 	{/if}*}
 	<li>&#187; <a href="{url op="people" path="reviewers"}">{*translate key="user.role.reviewers"*}ERC Members & External Reviewers</a></li>
 	<li>&#187; <a href="{url op="people" path="authors"}">{translate key="user.role.authors"}</a></li>
-	<li>&#187; <a href="{url op="showNoRole"}">{translate key="manager.people.showNoRole"}</a></li>
+	<!-- Not useful: EL on February 13th 2013 -->
+	<!--<li>&#187; <a href="{url op="showNoRole"}">{translate key="manager.people.showNoRole"}</a></li>-->
 	<!-- <li>&#187; <a href="{url op="people" path="readers"}">{translate key="user.role.readers"}</a></li> Edited by MSB, Nov17,2011-->
 	<!-- <li>&#187; <a href="{url op="people" path="subscriptionManagers"}">{translate key="user.role.subscriptionManagers"}</a></li> Edited by MSB, Nov17,2011-->
 	{call_hook name="Templates::Manager::Index::Roles"}
